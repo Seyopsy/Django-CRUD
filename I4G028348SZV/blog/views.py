b5f4d32bc70bb6d28d61e7fe89b92320d5e890b4
Seyopsy <seyopsy@gmail.com>
2022-06-26 15:44:36 +0100

    Commit Now

--- I4G028348SZV/blog/views.py
+++ I4G028348SZV/blog/views.py
@@ -1,44 +1,34 @@
-from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from blog.models import Post
-from .models import Blog
 
 # Create your views here.
 class PostListView(ListView):
     model=Post
-    template_name = "post_list.html"
+    template = "blog/post_list.html"
     
 class PostCreateView(CreateView):
     model=Post
-    fields=[
-    "__all__"
-    ]
-    template_name = "post_form.html"
+    fields="__all__"
+    template = "./blog/post_form.html"
+    # I4G028348SZV
+    # I4G028348SZV\blog\templates\blog\post_form.html
     success_url=reverse_lazy("blog:all")
 
 class PostDetailView(DetailView):
     model=Post
-    template_name= "post_detail.html"
+    template = "./blog/post_detail.html"
 
 class PostUpdateView(UpdateView):
     model=Post
-    fields=[
-    "__all__"
-    ]
-    template_name = "post_detail.html"
+    fields="__all__"
     success_url=reverse_lazy("blog:all")
+    template = "./blog/post_form.html"
 
 class PostDeleteView(DeleteView):
     model=Post
-    fields=[
-    "__all__"
-    ]
-    template_name = "post_confirm_delete.html"
+    fields="__all__"
     success_url=reverse_lazy("blog:all")
-
-
-
-
+    template = "./blog/post_confirm_delete.html"
